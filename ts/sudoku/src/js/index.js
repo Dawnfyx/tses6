2e78f6a831b9906778dd0b2405a7ddacbf50e567
Dawnfyx <15021677496@163.com>
2019-03-19 10:31:44 +0800

    文件目录

--- ts/sudoku/src/js/index.js
+++ ts/sudoku/src/js/index.js
@@ -6,9 +6,10 @@ function makeRow( v = 0){
 }
 
 function makeMatrix( v = 0 ){
-    const array = new Array(9);
-    array.fill(makeRow(v));
-    return array;
+    // const array = new Array(9);
+    // array.fill(makeRow(v));
+    // return array;
+    return Array.from({ length: 9}).map( () => makeRow(v));
 }
 
 const a = makeMatrix();
