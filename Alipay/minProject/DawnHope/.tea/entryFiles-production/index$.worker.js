062ca29f466bd5aa7cb32a113a7a38399aae4e16
Dawn <15021677496@163.com>
2019-08-12 19:22:52 +0800

    DawnHope
    蚂蚁小程序

---
+++ Alipay/minProject/DawnHope/.tea/entryFiles-production/index$.worker.js
@@ -0,0 +1,26 @@
+if(!self.__appxInited) {
+self.__appxInited = 1;
+
+
+require('./config$');
+require('./importScripts$');
+
+var AFAppX = self.AFAppX;
+self.getCurrentPages = AFAppX.getCurrentPages;
+self.getApp = AFAppX.getApp;
+self.Page = AFAppX.Page;
+self.App = AFAppX.App;
+self.my = AFAppX.bridge || AFAppX.abridge;
+self.abridge = self.my;
+self.Component = AFAppX.WorkerComponent || function(){};
+self.$global = AFAppX.$global;
+self.requirePlugin = AFAppX.requirePlugin;
+
+
+
+function success() {
+require('../../client/app');
+require('../../client/pages/index/index');
+}
+self.bootstrapApp ? self.bootstrapApp({ success }) : success();
+}
