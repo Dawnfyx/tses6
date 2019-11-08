01dfe158741d731af2cb48005bb78059b8a5ebb6
Dawn <15021677496@163.com>
2019-11-08 18:33:23 +0800

    提交

--- es6/tool-babelrc/dest/index.js
+++ es6/tool-babelrc/dest/index.js
@@ -1,4 +1,17 @@
 "use strict";
 
+var _axios = _interopRequireDefault(require("axios"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
 var a = 2;
 console.log(a);
+
+_axios["default"].post('/user', {
+  firstName: 'Fred',
+  lastName: 'Flintstone'
+}).then(function (response) {
+  console.log(response);
+})["catch"](function (error) {
+  console.log(error);
+});
--- es6/tool-babelrc/src/index.js
+++ es6/tool-babelrc/src/index.js
@@ -1,2 +1,15 @@
+import axios from "axios"
+
 let a = 2;
 console.log(a);
+
+axios.post('/user', {
+    firstName: 'Fred',
+    lastName: 'Flintstone'
+})
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+});
