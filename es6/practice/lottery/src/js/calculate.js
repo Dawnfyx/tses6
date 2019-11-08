494475c467ebaa996b9da4377a4fa95e1ffaaf62
Dawn <15021677496@163.com>
2019-11-09 01:14:14 +0800

    timer interface

--- es6/practice/lottery/src/js/calculate.js
+++ es6/practice/lottery/src/js/calculate.js
@@ -1,4 +1,14 @@
 class Calculate{
 
+    /**
+     * [computeCount 计算注数]
+     * @param  {number} active    [当前选中的号码]
+     * @param  {string} play_name [当前的玩法标识]
+     * @return {number}           [注数]
+     */
+    computeCount(active, play_name){
+
+    }
+
 }
 export default Calculate
