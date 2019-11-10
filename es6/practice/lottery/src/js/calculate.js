a4947dbdbdc972b8fffd30ae48a635d17aa0b966
Dawn <15021677496@163.com>
2019-11-11 00:03:52 +0800

    tijiao

--- es6/practice/lottery/src/js/calculate.js
+++ es6/practice/lottery/src/js/calculate.js
@@ -7,7 +7,24 @@ class Calculate{
      * @return {number}           [注数]
      */
     computeCount(active, play_name){
+        let count = 0;
+        const exist = this.play_list.has(paly_name);
+        const arr = new Array(active).fill("0")
+        if(exist && play_name.at(0) === "r"){
+            count = Calculate.combine(arr, play_name.split('')[1].length)
+        }
+        return  count
+    }
 
+    computeBonus(active, play_name){
+        const play = play_name.split('')
+        const self = this
+        let arr = new Array( play[1] * 1).fill( 0)
+        let min, max
+        if(play[0] === 'r'){
+            let min_active = 5 - (11 -active)
+
+        }
     }
 
 }
