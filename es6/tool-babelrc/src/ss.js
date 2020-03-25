b4dcc2dac84c35c48111092cfa5ad41c34d5f637
方亚希 <fangyx@ewpower.com.cn>
2020-03-25 13:41:56 +0800

    tijiao

---
+++ es6/tool-babelrc/src/ss.js
@@ -0,0 +1,35 @@
+import Notify from '@wcjiang/notify';
+
+const notify = new Notify({
+    message: '有消息了。', // 标题
+    effect: 'flash', // flash | scroll 闪烁还是滚动
+    openurl:'https://github.com/jaywcjlove/iNotify', // 点击弹窗打开连接地址
+    onclick: () => { // 点击弹出的窗之行事件
+        console.log('---')
+    },
+    // 可选播放声音
+    audio:{
+        // 可以使用数组传多种格式的声音文件
+        file: ['msg.mp4','msg.mp3','msg.wav']
+        // 下面也是可以的哦
+        // file: 'msg.mp4'
+    },
+    // 标题闪烁，或者滚动速度
+    interval: 1000,
+    // 可选，默认绿底白字的  Favicon
+    updateFavicon:{
+        // favicon 字体颜色
+        textColor: '#fff',
+        // 背景颜色，设置背景颜色透明，将值设置为“transparent”
+        backgroundColor: '#2F9A00'
+    },
+
+    // 可选chrome浏览器通知，默认不填写就是下面的内容
+    notification:{
+        title:'通知！', // 设置标题
+        icon:'', // 设置图标 icon 默认为 Favicon
+        body:'您来了一条新消息', // 设置消息内容
+    }
+});
+
+notify.player();
---
+++ ts/tslearn/enmu.ts
@@ -0,0 +1,10 @@
+enum OrderStatus{
+    Start = 1,
+    Unpaid,
+    Shipping,
+    Shipped,
+    Complete,
+}
+
+console.log(OrderStatus.Start);
+console.log(OrderStatus.Unpaid);
